e45a7f4ac93559655367fa243f7e4503aae0973c
knechtel <maiquelknechtel@gmail.com>
2022-06-12 21:33:42 -0300

    Adicionando ordem decrescente a lista de clientes.

--- dao/client_dao.py
+++ dao/client_dao.py
@@ -1,12 +1,14 @@
 
 import sqlalchemy
+from sqlalchemy import desc
 from models import Client
+
 from . import factory
 
 
 def find_all():
     sessionobj = factory.connect()
-    clients = sessionobj.query(Client).all()
+    clients = sessionobj.query(Client).order_by(desc((Client.id))).all()
     return clients
 
 
